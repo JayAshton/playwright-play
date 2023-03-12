f612cd583ab59fa7957e106e6ed78f23015f584e
Jay Ashton <j.c.j.ashton@gmail.com>
2023-03-12 19:37:36 +0000

    Add add/remove elements tests and linting

--- page-objects/add-remove-elements.po.ts
+++ page-objects/add-remove-elements.po.ts
@@ -1,13 +1,36 @@
 // playwright-dev-page.ts
-import { Page } from "@playwright/test";
+import { Page } from '@playwright/test';
 
 export class AddRemoveElementsPageObject {
-  readonly title = this.page.locator("#content", {
-    hasText: "Add/Remove Elements",
+  readonly title = this.page.locator('#content', {
+    hasText: 'Add/Remove Elements',
   });
-  readonly addElementButton = this.page.locator("button", {
-    hasText: "Add Element",
+  readonly addElementButton = this.page.locator('button', {
+    hasText: 'Add Element',
   });
+  readonly addedElement = this.page.locator('.added-manually');
 
   constructor(private page: Page) {}
+
+  async addElement(count?: number) {
+    if (count) {
+      for (let i = 0; i < count; i++) {
+        await this.addElementButton.click();
+      }
+      return;
+    }
+
+    await this.addElementButton.click();
+  }
+
+  async deleteElement(count?: number) {
+    if (count) {
+      for (let i = 0; i < count; i++) {
+        await this.addedElement.nth(0).click();
+      }
+      return;
+    }
+
+    await this.addedElement.click();
+  }
 }
---
+++ specs/add-remove-elements.spec.ts
@@ -0,0 +1,37 @@
+import { expect } from '@playwright/test';
+import test from '../fixtures';
+
+test.describe('Adding and removing elements from the DOM', () => {
+  test.beforeEach(async ({ page }) => {
+    // Go to the baseUrl, with the extra path
+    await page.goto('add_remove_elements/');
+  });
+
+  test('Title is present', async ({ addRemoveElementsPage }) => {
+    await expect(addRemoveElementsPage.title).toBeVisible();
+  });
+
+  test('Add and delete an element', async ({ addRemoveElementsPage }) => {
+    await addRemoveElementsPage.addElement();
+    await expect.soft(addRemoveElementsPage.addedElement).toBeVisible();
+    expect.soft(await addRemoveElementsPage.addedElement.count()).toEqual(1);
+
+    await addRemoveElementsPage.deleteElement();
+    await expect.soft(addRemoveElementsPage.addedElement).not.toBeVisible();
+    expect.soft(await addRemoveElementsPage.addedElement.count()).toEqual(0);
+  });
+
+  test('Add and delete multiple elements', async ({
+    addRemoveElementsPage,
+  }) => {
+    const numberOfElementsToAdd = 3;
+
+    await addRemoveElementsPage.addElement(numberOfElementsToAdd);
+    expect
+      .soft(await addRemoveElementsPage.addedElement.count())
+      .toEqual(numberOfElementsToAdd);
+
+    await addRemoveElementsPage.deleteElement(numberOfElementsToAdd);
+    expect.soft(await addRemoveElementsPage.addedElement.count()).toEqual(0);
+  });
+});
