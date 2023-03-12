dbe46b48406c4a0b7ab197077be0645dcb18b868
Jay Ashton <j.c.j.ashton@gmail.com>
2023-03-12 18:59:36 +0000

    Additional specs

--- specs/search.spec.ts
+++ specs/search.spec.ts
@@ -1,13 +1,13 @@
 import { expect } from "@playwright/test";
 import test from "../fixtures";
 
-test.describe("IMDB Homepage", () => {
+test.describe("IMDB Search", () => {
   test.beforeEach(async ({ page }) => {
     // Go to the baseUrl as defined in playwright config
     await page.goto("");
   });
 
-  test("Logo is present on homepage", async ({ homePage }) => {
+  test("Generic search returns relevant results", async ({ homePage }) => {
     await expect(homePage.logo).toBeVisible();
   });
 });
