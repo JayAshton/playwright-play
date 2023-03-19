c7210cd974e16eb7326958d7034043a0529d33bc
Jay Ashton <j.c.j.ashton@gmail.com>
2023-03-19 18:58:57 +0000

    fixes

--- specs/broken-images.spec.ts
+++ specs/broken-images.spec.ts
@@ -12,11 +12,11 @@ test.describe('Broken Images', () => {
 
   test('All images have a src attribute', async ({ brokenImagesPage }) => {
     for (const image of await brokenImagesPage.images.all()) {
-      expect(await image.getAttribute('src')).not.toBeNull;
+      expect(await image.getAttribute('src')).not.toBeNull();
     }
   });
 
-  test.only('2 broken images (404 not found)', async ({ page }) => {
+  test('2 broken images (404 not found)', async ({ page }) => {
     const errorLogs: string[] = [];
     const errorMessage =
       'the server responded with a status of 404 (Not Found)';
--- specs/checkboxes.spec.ts
+++ specs/checkboxes.spec.ts
@@ -7,17 +7,17 @@ test.describe('Checkboxes', () => {
   });
 
   test('Checkbox 2 is checked by default', async ({ checkboxPage }) => {
-    expect(await checkboxPage.checkboxTwo.isChecked()).toBeTruthy;
+    expect(await checkboxPage.checkboxTwo.isChecked()).toBeTruthy();
   });
 
   test('Checkbox 1 can be checked', async ({ checkboxPage }) => {
     await checkboxPage.checkboxOne.check();
-    expect(await checkboxPage.checkboxOne.isChecked()).toBeTruthy;
+    expect(await checkboxPage.checkboxOne.isChecked()).toBeTruthy();
   });
 
   test('Checkbox 2 can be unchecked', async ({ checkboxPage }) => {
     await checkboxPage.checkboxTwo.uncheck();
-    expect(await checkboxPage.checkboxTwo.isChecked()).toBeFalsy;
+    expect(await checkboxPage.checkboxTwo.isChecked()).toBeFalsy();
   });
 
   test('Both checkboxes can be checked at the same time', async ({
